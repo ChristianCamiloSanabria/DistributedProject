355f337b8a561841d8ece8dd9674edab0811f199
gina <castillogina24@gmail.com>
2022-06-17 17:24:08 -0500

    service get student.js

--- src/routes/services.js
+++ src/routes/services.js
@@ -1,47 +1,68 @@
 import Task from "../schema/task.js";
 import index from "../views/index_html.js";
 import express from "express";
-
+import Student from "../schema/student.js";
 
 const router = express.Router();
 
 
+router.get("/cuenta/:idcuenta/:idPersona", (req, res) => {
+    console.log(req.params);
+    let va = req.params.idPersona;
+    console.log(req.params.idcuenta);
+    console.log(va);
 
-router.get("/cuenta/:idcuenta/:idPersona",(req,res)=>{
-	console.log(req.params);
-	console.log(req.params.idcuenta);
-	console.log(req.params.idPersona);
-	res.send("Tu cuenta personal");
+    res.send("Tu cuenta personal: " + req.params.idPersona);
+});
+/**
+ *
+ */
+router.get("/students/:id_student", async (req, res) => {
+    console.log(req.params);
+    let va = req.params.id_student;
+    console.log(va);
+    const tasks_db = await Student.find({"id_student": va});
+    console.log(tasks_db);
+    res.send("Estudiante: " + req.params.id_student);
+
 });
 
 
 //Metodos GET
 
 /**
-	xde
-**/
-router.get('/',async (req, res) => {
-	//Aqui estoy recogiendo los datos del servidor
-	const tasks_db = await Task.find();
-	console.log(tasks_db);
-	/*Aqui es donde se le pasa dos parametros al navegador 
-		Primer_Parametro: index que viene el html
-		Segundo_Parametro: El objeto .JSON que viene por tasks_db
-	
-	res.render("index",{
-		tasks: tasks_db
-	}); 
-	*/
-	//res.send('Hollo world!');
-	//console.log(index);
-	//res.send(index);
-	res.send("Hola");
+ xde
+ **/
+/*
+router.get('/', async (req, res) => {
+    //Aqui estoy recogiendo los datos del servidor
+    const tasks_db = await Task.find();
+    console.log(tasks_db);
+    const task = {
+        title: "Juan",
+        description: "344",
+        status: true
+    };
+    const task3 = await Task.send(task);
+    const task2 = await Task.save();
+    res.status(201).send({task});
+
+
+    /!*Aqui es donde se le pasa dos parametros al navegador
+        Primer_Parametro: index que viene el html
+        Segundo_Parametro: El objeto .JSON que viene por tasks_db
+
+    res.render("index",{
+        tasks: tasks_db
+    });
+    *!/
+    //res.send('Hollo world!');
+    //console.log(index);
+    //res.send(index);
+    res.send("Hola");
 
 });
-
-
-
-
+*/
 
 
 export default router;
