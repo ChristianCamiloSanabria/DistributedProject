a00ff721f2de21708ca0bbabf079f6d8a33abdc1
MichaelRodriguez06 <michael.rodriguez06@uptc.edu.co>
2022-06-20 21:14:22 -0500

    create schemas subject.js and inscription.js

--- src/routes/services.js
+++ src/routes/services.js
@@ -1,6 +1,6 @@
 import Task from "../schema/task.js";
-import index from "../views/index_html.js";
 import express from "express";
+import Student from "../schema/student.js";
 
 
 const router = express.Router();
@@ -14,35 +14,20 @@ router.get("/cuenta/:idcuenta/:idPersona", (req, res) => {
 });
 
 
-//Metodos GET
-
-/*
-/!**
-
- **!/
-router.get('/', async (req, res) => {
-    //Aqui estoy recogiendo los datos del servidor
-    const tasks_db = await Task.find();
-    console.log(tasks_db);
-    /!*Aqui es donde se le pasa dos parametros al navegador
-        Primer_Parametro: index que viene el html
-        Segundo_Parametro: El objeto .JSON que viene por tasks_db
-
-    res.render("index",{
-        tasks: tasks_db
-    });
-    *!/
-    //res.send('Hollo world!');
-    //console.log(index);
-    //res.send(index);
-    res.send("Hola");
-
-});
-*/
 
 /**
- ** GET
- **
+ URL vacia
+ **/
+router.get('/', async (req, res) => {
+    const tasks_db = await Task.find();
+    console.log(tasks_db);
+        res.send("Hola");
+});
+
+/**
+ * GET:
+ *  Obtiene el ultimo estudiante guardado en la base de datos usando
+ *  la URL /show/last_saved_student
  **/
 router.get("/show/last_saved_student", async (req, res) => {
     const listStudent = await Student.find();
@@ -61,7 +46,7 @@ router.get("/show/last_saved_student", async (req, res) => {
  **
  **/
 
-router.post("/add/:id_student/:number_document/:name_student/:type_document/:lastname_student/:code_student", async (req, res) => {
+router.post("/addStudent/:id_student/:number_document/:name_student/:type_document/:lastname_student/:code_student", async (req, res) => {
     console.log(req.params);
     const student = new Student({
         "id_student": req.params.id_student,
@@ -75,4 +60,47 @@ router.post("/add/:id_student/:number_document/:name_student/:type_document/:las
     console.log(student);
     res.status(200).send("Post: Saved Student" + await student.save());
 });
+
+
+/***
+ * Metodos PUT
+ */
+
+
+/***
+ * PUT Estudiante
+ */
+router.put('put/:id_student/:number_document/:type_document/:name_student/:lastname_student/:code_student', async (req, res) => {
+    try {
+        const id = Number(req.params.id_student);
+        const number_document = Number(req.params.number_document);
+        const type_document = req.params.type_document;
+        const name_student = req.params.name_student;
+        const lastname_student = req.params.lastname_student;
+        const code_student = req.params.code_student;
+        let response = await db
+    } catch (error) {
+        res.send(error);
+    }
+
+});
+
+/***
+ * PUT materia
+ */
+router.put('put/:id_student/:number_document/:type_document/:name_student/:lastname_student/:code_student', async (req, res) => {
+    try {
+        const id = Number(req.params.id_student);
+        const number_document = Number(req.params.number_document);
+        const type_document = req.params.type_document;
+        const name_student = req.params.name_student;
+        const lastname_student = req.params.lastname_student;
+        const code_student = req.params.code_student;
+        let response = await db
+    } catch (error) {
+        res.send(error);
+    }
+
+});
+
 export default router;
