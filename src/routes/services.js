0bbd3e010730570c6e18bddd506896c9568dd3a6
ChristianCamiloSanabria <christian.sanabria1988@gmail.com>
2022-06-21 07:24:41 -0500

    Actualizacion de metodo post add student con validacion de error y validacion de duplicacion de estudiante

--- src/routes/services.js
+++ src/routes/services.js
@@ -63,74 +63,59 @@ router.get("/show/last_saved_student",async (req, res) => {
 });
 
 
+router.get("/show/list_student",async (req, res) => {
+	const listStudent = await Student.find();
+	var students ="";
+	listStudent.forEach(function(element) {
+		students += element;
+		console.log(students);
+	});
+
+	res.status(200).send("GET::"+students);
+});
 
 
+ 
 /**
 ** POST
-**
+** Servicio de insertar a la DB un estudiante. 
 **/
 
-router.post("/add/:id_student/:number_document/:name_student/:type_document/:lastname_student/:code_student",async (req, res) => {
-	console.log(req.params);
-	const student = new Student({"id_student": req.params.id_student,"number_document": req.params.number_document,"type_document": '"'+req.params.type_document+'"',"name_student": '"'+req.params.name_student+'"',"lastname_student": '"'+req.params.lastname_student+'"',"code_student": req.params.code_student,"status": "true"});
-	console.log(student);
-	res.status(200).send("Post: Saved Student"+ await student.save());
+router.post("/add/:id_student/:number_document/:type_document/:name_student/:lastname_student/:code_student",async (req, res) => {
+	try{
+		console.log("Aqui llegan los parametros"+req.params);
+		const listStudent = await Student.find();
+		if(checkStudent(req.params,listStudent)){
+			//const student = new Student({"id_student": req.params.id_student,"number_document": req.params.number_document,"type_document": '"'+req.params.type_document+'"',"name_student": '"'+req.params.name_student+'"',"lastname_student": '"'+req.params.lastname_student+'"',"code_student": req.params.code_student,"status": "true"});
+			const student = new Student(req.params);
+			console.log("Aqui se crea el estudiante:"+student);
+			res.status(200).send("Post: Saved Student"+ await student.save());	
+		}else{
+			res.status(200).send("El estudiante ya se encuantra registrado");
+		}
+	} catch(err){
+      console.error(err); //mostramos el error por consola para poder solucionar futuros errores
+      res.status(500).send("error"); //en caso de error respondemos al cliente con un 500
+    }
 });
-
-
-
 
 /**
-router.post("/add/:id_student/:number_document/:name_student/:type_document/:lastname_student/:code_student",async (req, res) => {
-	console.log(req.params);
-
-	const student = new Student({"id_student": req.params.id_student,"number_document": req.params.number_document,"type_document": '"'+req.params.type_document+'"',"name_student": '"'+req.params.name_student+'"',"lastname_student": '"'+req.params.lastname_student+'"',"code_student": req.params.code_student,"status": "true"});
-	console.log(student);
-	await student.save();
-	
-
-	//Aqui estoy recogiendo los datos del servidor
-	//const student_db = new Student(null,"{_id: 12}","{id_student: 12,number_document: 1049,type_document: 'carnet',name_student: 'christian',lastname_student: 'Sanabria',code_student: 2332,status: 'true'");
-	//const student_db = new Student();
-	//console.log("Esto es la respuesta del Mongo:  "+await student_db.save());
-	//console.log(new Student(null,null,"{id_student: 12,number_document: 1049,type_document: 'carnet',name_student: 'christian',lastname_student: 'Sanabria',code_student: 2332,status: 'true'"));
-	
-	const listStudent = await Student.find();
-
-	listStudent.forEach(function(element) {
-		const v = element;
-	  console.log(v);
-	});
-
-
-	Aqui es donde se le pasa dos parametros al navegador 
-		Primer_Parametro: index que viene el html
-		Segundo_Parametro: El objeto .JSON que viene por tasks_db
-	
-	res.render("index",{
-		tasks: tasks_db
-	}); 
-
-	//res.send('Hollo world!');
-	//console.log(index);
-	//res.send(index);
-	console.log("Esto es lo que quiero mostrar: " + req);
-
-	req.forEach(function(element) {
-		const v = element;
-	  console.log(v);
-	});
-	//res.status(status).send(req.body);
-	res.status(200).send("Post");
-
-});
-
+** function checkStudent:
+** Verifica si un estudiante ya se encuentra registrado en la base de datos.
+** Return: un boleano que confirma si el estudiante se encuentra o no.
+** Parametros de entrada: 
+	student: Representa los datos del estudiante que se quiere insertar en la DB,
+	listStudent: Listado de todos lo estudiantes actuales en la DB.
 **/
-
-
-
-
-
+function checkStudent(student,listStudent){
+	let bolean= true;
+	listStudent.forEach(function(element) {
+		if (element.name_student==student.name_student&&element.lastname_student==student.lastname_student) {
+			bolean = false;
+		}
+	});
+	return bolean;
+}
 
 
 export default router;
